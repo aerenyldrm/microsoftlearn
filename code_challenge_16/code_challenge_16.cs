a1fee867c1ced2c86a19993f0121bfc1ee367600
aerenyldrm <aerenyldrm@outlook.com>
2024-08-23 19:44:51 +0300

    update

---
+++ code_challenge_16/code_challenge_16.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+string message = "MESSAGE: ";
+float total = 0;
+
+foreach(string value in values)
+{
+    float checkFloat;
+    if (float.TryParse(value, out checkFloat))
+        total += checkFloat;
+    else 
+        message += value;
+}
+
+Console.WriteLine(message);
+Console.WriteLine($"TOTAL: {total}");
--- develop_conditional_branching_and_looping_structures_in_cs/obj/Debug/net8.0/develop_conditional_branching_and_looping_structures_in_cs.AssemblyInfo.cs
+++ develop_conditional_branching_and_looping_structures_in_cs/obj/Debug/net8.0/develop_conditional_branching_and_looping_structures_in_cs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("develop_conditional_branching_and_looping_structures_in_cs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91a30912f9a605530c5688910b3fc0c1fd9588c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("develop_conditional_branching_and_looping_structures_in_cs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("develop_conditional_branching_and_looping_structures_in_cs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
