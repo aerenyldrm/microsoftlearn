202df6faef966c61205cc8685f144220bfd94255
aerenyldrm <aerenyldrm@outlook.com>
2024-08-30 23:25:17 +0300

    update

---
+++ code_challenge_21/code_challenge_21.cs
@@ -0,0 +1,27 @@
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// FIND QUANTITY
+string opening = "<span>";
+string closing = "</span>";
+int openingPosition = input.IndexOf(opening);
+int closingPosition = input.IndexOf(closing);
+openingPosition += opening.Length;
+int length = closingPosition - openingPosition;
+
+quantity = $"QUANTITY: {input.Substring(openingPosition, length)}";
+
+// FIND OUTPUT
+opening = "<div>";
+closing = "</div>";
+openingPosition = input.IndexOf(opening);
+closingPosition = input.IndexOf(closing);
+openingPosition += opening.Length;
+length = closingPosition - openingPosition;
+
+output = $"OUTPUT: {input.Substring(openingPosition, length).Replace("trade", "reg")}";
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
