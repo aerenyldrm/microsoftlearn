861d77ba62c7fbe599733ad91b2f9c18c8b2f9d6
aerenyldrm <aerenyldrm@outlook.com>
2024-09-04 23:41:04 +0300

    update

---
+++ plan_a_petting_zoo_visit/plan_a_petting_zoo_visit.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+PlanSchoolVisit("SCHOOL A");
+PlanSchoolVisit("SCHOOL B", 3);
+PlanSchoolVisit("SCHOOL C", 2);
+
+void PlanSchoolVisit(string schoolName, int groups = 6)
+{
+    RandomizeAnimals();
+    string[,] group = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group);
+}
+
+void RandomizeAnimals()
+{
+    Random random = new();
+
+    for (int i = 0; i < pettingZoo.Length; i++)
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[i];
+        pettingZoo[i] = pettingZoo[r];
+        pettingZoo[r] = temp;
+    }
+}
+
+string [,] AssignGroup(int groups = 6)
+{
+    string[,] result = new string[groups, pettingZoo.Length / groups];
+
+    int start = 0;
+
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+            result[i, j] = pettingZoo[start++];
+    }
+
+    return result;
+}
+
+void PrintGroup(string[,] group)
+{
+    for (int i = 0; i < group.GetLength(0); i++)
+    {
+        Console.Write($"GROUP {i + 1}: ");
+        for (int j = 0; j < group.GetLength(1); j++)
+            Console.Write($"{group[i, j]} ");
+        Console.WriteLine();
+    }
+}
