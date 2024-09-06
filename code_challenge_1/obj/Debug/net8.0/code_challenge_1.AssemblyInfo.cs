7704d945f09afb9365e77f66da4cf51613bad3e8
aerenyldrm <aerenyldrm@outlook.com>
2024-09-06 07:44:54 +0300

    update

--- code_challenge_1/obj/Debug/net8.0/code_challenge_1.AssemblyInfo.cs
+++ code_challenge_1/obj/Debug/net8.0/code_challenge_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("code_challenge_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa78c74d3b13c43bcd6ed083844bf0096953491c")]
 [assembly: System.Reflection.AssemblyProductAttribute("code_challenge_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("code_challenge_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
