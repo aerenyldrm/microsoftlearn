ad94af65a95fd09d95f1feb3f5ad9c447ee7a438
aerenyldrm <aerenyldrm@outlook.com>
2024-09-07 15:08:19 +0300

    update

--- challenge_project_daccauvsc/obj/Debug/net8.0/challenge_project_daccauvsc.AssemblyInfo.cs
+++ challenge_project_daccauvsc/obj/Debug/net8.0/challenge_project_daccauvsc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("challenge_project_daccauvsc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba314946c60751437a6516a747ea44474cb521d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("challenge_project_daccauvsc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("challenge_project_daccauvsc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
